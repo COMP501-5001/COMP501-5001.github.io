fb3e802072ea1966a74b46a8a59b4ee4a6a9b2ea
fujimite <fujimite@users.noreply.github.com>
2022-09-22 18:43:08 +1200

    add js for menu

---
+++ script.js
@@ -0,0 +1,38 @@
+/* sidebar menu c/o w3schools.com */
+
+/*
+function openNav() {
+    document.getElementById("sidebarMenu").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+    document.getElementById("sidebarMenu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+*/
+
+function setNav() {
+    document.getElementById("sidebarMenu").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function toggleNav() {
+    var x = document.getElementById("sidebarMenu");
+    var icon = document.getElementById("menuButtonIcon")
+    if (x.style.width === "0px") {
+      x.style.width = "250px";
+      icon.innerHTML = "close";
+    } else {
+      x.style.width = "0px";
+      icon.innerHTML = "menu";
+    }
+    var x = document.getElementById("main");
+    if (x.style.marginLeft === "0px") {
+      x.style.marginLeft = "250px";
+      icon.innerHTML = "close";
+    } else {
+      x.style.marginLeft = "0px";
+      icon.innerHTML = "menu";
+    }
+  }
