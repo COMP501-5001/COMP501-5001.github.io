6678c1c0b7d5dd461f42eb2a507577f0ab8b6a6f
fujimite <fujimite@users.noreply.github.com>
2022-09-22 20:36:05 +1200

    add site info, rename stylesheet

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 /* sidebar menu c/o w3schools.com */
 
-/*
-function openNav() {
-    document.getElementById("sidebarMenu").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-  
-function closeNav() {
-    document.getElementById("sidebarMenu").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-}
-*/
-
 function setNav() {
     document.getElementById("sidebarMenu").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
