a33ab0e9999401eef11dedb8091cc3698568368b
fujimite <fujimite@users.noreply.github.com>
2022-10-04 19:39:43 +1300

    update alert message

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,5 @@ function clearStorage()
 {
     sessionStorage.clear();
     localStorage.clear();
-    alert("All data stored by this website has been cleared. However, reloading or accessing another page will cause new data to be created. If you don't want this, close the website after closing this alert message.");
+    alert("All data stored by this website has been cleared. However, reloading or accessing another page on this website will cause new data to be created. If you don't want this, close the website after closing this alert message.");
 }
